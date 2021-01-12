3c4788a23b7535de9a37dff8a57b44b3d0dc9fa8
jhiat00 <jessehiatt4@gmail.com>
2021-01-12 15:03:19 -0600

    added protocol template

---
+++ Protocol.xctemplate/___FILEBASENAME___.swift
@@ -0,0 +1,22 @@
+//___FILEHEADER___
+
+import Foundation
+
+extension Environment: ___FILEBASENAMEASIDENTIFIER___Factory {
+    func make___FILEBASENAMEASIDENTIFIER___() -> ___FILEBASENAMEASIDENTIFIER___Protocol {
+        return ___FILEBASENAMEASIDENTIFIER___()
+    }
+}
+
+protocol ___FILEBASENAMEASIDENTIFIER___Factory {
+    func make___FILEBASENAMEASIDENTIFIER___() -> ___FILEBASENAMEASIDENTIFIER___Protocol
+}
+
+protocol ___FILEBASENAMEASIDENTIFIER___Protocol {
+    
+}
+
+class ___FILEBASENAMEASIDENTIFIER___ {}
+extension ___FILEBASENAMEASIDENTIFIER___: ___FILEBASENAMEASIDENTIFIER___Protocol {
+    
+}
